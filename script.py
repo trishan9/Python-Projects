7c370786edaf9731a1b4fbf805f2fb30f4407213
Trishan Wagle <mailtotrishan@gmail.com>
2023-09-23 15:12:24 +0545

    feat: added python projects.

---
+++ script.py
@@ -0,0 +1,6 @@
+import os
+
+projects = ["BMI-Calculator", "Love-Calculator", "PizzaBill-Calculator", "Treasure-Finding-Game", "Password-Generator", "Rock-Paper-Scissors", "Hangman-Game", "Kaun-Banega-Crorepati", "Auction", "BlackJack-Game", "Calculator", "Guess-The-Number", "Coffee-Machine", "Higher-Lower-Game", "Secret-Code", "Quiz-Game-OOP"]
+
+for project in projects:
+    os.mkdir(f"{project}")
