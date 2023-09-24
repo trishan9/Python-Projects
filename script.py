0e9cd639edefcd27bb394dba8e6ef7cb5e60b502
Trishan Wagle <mailtotrishan@gmail.com>
2023-09-24 18:56:27 +0545

    feat: added turtle racing game.

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import os
 
-projects = ["BMI-Calculator", "Love-Calculator", "PizzaBill-Calculator", "Treasure-Finding-Game", "Password-Generator", "Rock-Paper-Scissors", "Hangman-Game", "Kaun-Banega-Crorepati", "Auction", "BlackJack-Game", "Calculator", "Guess-The-Number", "Coffee-Machine", "Higher-Lower-Game", "Secret-Code", "Quiz-Game-OOP"]
+projects = ["BMI-Calculator", "Love-Calculator", "PizzaBill-Calculator", "Treasure-Finding-Game", "Password-Generator", "Rock-Paper-Scissors", "Hangman-Game", "Kaun-Banega-Crorepati", "Auction", "BlackJack-Game", "Calculator", "Guess-The-Number", "Coffee-Machine", "Higher-Lower-Game", "Secret-Code", "Quiz-Game-OOP", "Turtle-Racing-Game"]
 
 for project in projects:
-    os.mkdir(f"{project}")
+    if not os.path.exists(project):
+        os.mkdir(f"{project}")
