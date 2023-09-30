655c9fd9822ea420d4b24a03036695be34d01f67
Trishan Wagle <mailtotrishan@gmail.com>
2023-09-30 12:37:25 +0545

    feat: added us states guessing game.

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import os
 
-projects = ["BMI-Calculator", "Love-Calculator", "PizzaBill-Calculator", "Treasure-Finding-Game", "Password-Generator", "Rock-Paper-Scissors", "Hangman-Game", "Kaun-Banega-Crorepati", "Auction", "BlackJack-Game", "Calculator", "Guess-The-Number", "Coffee-Machine", "Higher-Lower-Game", "Secret-Code", "Quiz-Game-OOP", "Turtle-Racing-Game"]
+projects = ["BMI-Calculator", "Love-Calculator", "PizzaBill-Calculator", "Treasure-Finding-Game", "Password-Generator", "Rock-Paper-Scissors", "Hangman-Game", "Kaun-Banega-Crorepati", "Auction", "BlackJack-Game", "Calculator", "Guess-The-Number", "Coffee-Machine", "Higher-Lower-Game", "Secret-Code", "Quiz-Game-OOP", "Turtle-Racing-Game", "U.S-States-Game"]
 
 for project in projects:
     if not os.path.exists(project):
